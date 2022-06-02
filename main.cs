c109a36337dca208173cb4bc060ffd5106223350
Joanne Santhosh <99355156+joannesanthosh@users.noreply.github.com>
2022-06-02 00:25:02 +0000

    fix main.cs

--- main.cs
+++ main.cs
@@ -9,42 +9,41 @@ class Program
 {
     public static void Main(string[] args)
     {
+      int firstInteger;
+      int secondInteger;
+      var addedNumber = 0;
+      var result = 0;
 
-        int firstInteger;
-        int secondInteger;
-        var addedNumber = 0;
-        var result = 0;
-
-        // input
-        Console.Write("This program multiplies numbers using loops");
-        Console.WriteLine("");
-        Console.Write("Enter the first integer: ");
-        firstInteger = Convert.ToInt32(Console.ReadLine());
-        Console.Write("Enter the second integer: ");
-        secondInteger = Convert.ToInt32(Console.ReadLine());
+      // input
+      Console.Write("This program multiplies numbers using loops");
+      Console.WriteLine("");
+      Console.Write("Enter the first integer: ");
+      firstInteger = Convert.ToInt32(Console.ReadLine());
+      Console.Write("Enter the second integer: ");
+      secondInteger = Convert.ToInt32(Console.ReadLine());
       
         // process
-    if (firstInteger > 0 && secondInteger > 0) {
-      while (addedNumber < secondInteger) {
-        addedNumber = addedNumber + 1;
-        result = result + firstInteger;
-      }
-    } else if (firstInteger < 0 && secondInteger < 0) {
-      while (addedNumber > secondInteger) {
-        addedNumber = addedNumber - 1;
-        result = result - firstInteger;
-      }
-    } else if (firstInteger > 0 && secondInteger < 0) {
-        while (addedNumber > secondInteger) {
-        addedNumber = addedNumber - 1;
-        result = result - firstInteger;
-      }
-    } else if (firstInteger < 0 && secondInteger > 0) {
+       if (firstInteger > 0 && secondInteger > 0) {
         while (addedNumber < secondInteger) {
-        addedNumber = addedNumber + 1;
-        result = result + firstInteger;
+          addedNumber = addedNumber + 1;
+         result = result + firstInteger;
+        }
+      } else if (firstInteger < 0 && secondInteger < 0) {
+         while (addedNumber > secondInteger) {
+           addedNumber = addedNumber - 1;
+          result = result - firstInteger;
+        }
+      } else if (firstInteger > 0 && secondInteger < 0) {
+          while (addedNumber > secondInteger) {
+            addedNumber = addedNumber - 1;
+            result = result - firstInteger;
+         }
+      } else if (firstInteger < 0 && secondInteger > 0) {
+          while (addedNumber < secondInteger) {
+            addedNumber = addedNumber + 1;
+            result = result + firstInteger;
+         }
       }
-    }
         Console.WriteLine("The answer is: " + firstInteger + " x " + addedNumber + " = " + result);
     }
 }
